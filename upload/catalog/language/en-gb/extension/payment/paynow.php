7272096dcd99beb3a8acb472f2e1a7ccf4824f31
michalskladanowski <59174285+michalskladanowski@users.noreply.github.com>
2020-12-01 09:16:00 +0100

    Change payment method description (#12)

--- upload/catalog/language/en-gb/extension/payment/paynow.php
+++ upload/catalog/language/en-gb/extension/payment/paynow.php
@@ -4,6 +4,6 @@
 $_['heading_title'] = 'Paynow';
 
 // Text
-$_['payment_paynow_text_title'] = 'Pay by Paynow';
+$_['payment_paynow_text_title'] = 'Pay by paynow.pl';
 $_['payment_paynow_text_error'] = 'An error occurred while processing your payment.';
 $_['payment_paynow_text_order'] = 'Order: ';
--- upload/catalog/language/pl-pl/extension/payment/paynow.php
+++ upload/catalog/language/pl-pl/extension/payment/paynow.php
@@ -4,6 +4,6 @@
 $_['heading_title'] = 'Paynow';
 
 // Text
-$_['payment_paynow_text_title'] = 'Zapłać z Paynow';
+$_['payment_paynow_text_title'] = 'Zapłać z paynow.pl';
 $_['payment_paynow_text_error'] = 'Podczas płatności wystąpił błąd. Skontaktuj się z obsługą sklepu lub spróbuj ponownie.';
 $_['payment_paynow_text_order'] = 'Zamówienie: ';
